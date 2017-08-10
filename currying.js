42887d956b0e2709de77f757c1850a7e91c58fad
Frank Iriarte <jurrish@gmail.com>
2017-08-10 12:21:09 -0700

    another curry example

--- currying.js
+++ currying.js
@@ -12,3 +12,23 @@ add(12)(2);
 add12 = add(2);
 
 add12(5) //returns 17
+
+
+// =============================
+greet = (name, end) => console.log(name, end);
+
+greet = (name) => (end) => console.log(name, end);
+
+now greet can be invoked as greet('whomever')('with whateverend');
+
+es5:
+
+function greet(name) {
+  return function log(end) {
+    console.log(name, end)
+  }
+};
+
+greet('JR')('knows currying kinda');
+
+//the functions are just arguments
